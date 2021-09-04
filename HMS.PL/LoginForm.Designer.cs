923fc1d035369e22a958b2e6d63658130a827a1a
DulithaSandaruwan <dulithasandaruwan@gmail.com>
2021-09-04 09:49:19 +0530

    adding add button for loging page

--- HMS.PL/LoginForm.Designer.cs
+++ HMS.PL/LoginForm.Designer.cs
@@ -29,13 +29,24 @@ namespace HMS.PL
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(441, 145);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // LoginForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(987, 517);
+            this.Controls.Add(this.button1);
             this.Name = "LoginForm";
             this.Text = "LoginForm";
             this.ResumeLayout(false);
@@ -43,5 +54,7 @@ namespace HMS.PL
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
