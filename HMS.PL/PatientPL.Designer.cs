516694b8f688e241a7ee05003268c010db51087d
Nipuna Sanjaya <djnipuna1@gmail.com>
2021-09-05 20:14:30 +0530

    btn

--- HMS.PL/PatientPL.Designer.cs
+++ HMS.PL/PatientPL.Designer.cs
@@ -28,12 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(198, 199);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(320, 77);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // PatientPL
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Name = "PatientPL";
             this.Text = "PatientPL";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
