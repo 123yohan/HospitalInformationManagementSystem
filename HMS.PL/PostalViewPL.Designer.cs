4d69d2ae64bb149bb9237f73b4c40be6e14b3e0c
yohan <yohan5apple@gmail.com>
2021-10-10 19:11:17 +0530

    All

---
+++ HMS.PL/PostalViewPL.Designer.cs
@@ -0,0 +1,240 @@
+﻿
+namespace HMS.PL
+{
+    partial class PostalViewPL
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PostalViewPL));
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAddPostal = new System.Windows.Forms.Button();
+            this.dgvPostal = new System.Windows.Forms.DataGridView();
+            this.Edite = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Delete = new System.Windows.Forms.DataGridViewButtonColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPostal)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExit.FlatAppearance.BorderSize = 0;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Image = ((System.Drawing.Image)(resources.GetObject("btnExit.Image")));
+            this.btnExit.Location = new System.Drawing.Point(1145, 3);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(41, 35);
+            this.btnExit.TabIndex = 17;
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 19);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Postal Details";
+            // 
+            // btnAddPostal
+            // 
+            this.btnAddPostal.FlatAppearance.BorderSize = 0;
+            this.btnAddPostal.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAddPostal.Image = global::HMS.PL.Properties.Resources.icons8_add_new_30;
+            this.btnAddPostal.Location = new System.Drawing.Point(131, 6);
+            this.btnAddPostal.Name = "btnAddPostal";
+            this.btnAddPostal.Size = new System.Drawing.Size(31, 32);
+            this.btnAddPostal.TabIndex = 19;
+            this.btnAddPostal.UseVisualStyleBackColor = true;
+            this.btnAddPostal.Visible = false;
+            this.btnAddPostal.Click += new System.EventHandler(this.btnAddPostal_Click);
+            // 
+            // dgvPostal
+            // 
+            this.dgvPostal.AllowUserToAddRows = false;
+            this.dgvPostal.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.Orange;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dgvPostal.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvPostal.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvPostal.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvPostal.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPostal.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Edite,
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column6,
+            this.Column7,
+            this.Column5,
+            this.Delete});
+            this.dgvPostal.Location = new System.Drawing.Point(14, 44);
+            this.dgvPostal.Name = "dgvPostal";
+            this.dgvPostal.ReadOnly = true;
+            this.dgvPostal.RowHeadersVisible = false;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.White;
+            this.dgvPostal.RowsDefaultCellStyle = dataGridViewCellStyle3;
+            this.dgvPostal.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvPostal.Size = new System.Drawing.Size(1159, 474);
+            this.dgvPostal.TabIndex = 20;
+            this.dgvPostal.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvPostal_CellClick);
+            // 
+            // Edite
+            // 
+            this.Edite.HeaderText = "EDITE";
+            this.Edite.Name = "Edite";
+            this.Edite.ReadOnly = true;
+            this.Edite.Text = "EDITE";
+            this.Edite.ToolTipText = "Edite";
+            this.Edite.UseColumnTextForButtonValue = true;
+            // 
+            // Column1
+            // 
+            this.Column1.DataPropertyName = "Id";
+            this.Column1.HeaderText = "ID";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Visible = false;
+            // 
+            // Column2
+            // 
+            this.Column2.DataPropertyName = "IsReceivedMail";
+            this.Column2.HeaderText = "Mail Type";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Width = 150;
+            // 
+            // Column3
+            // 
+            this.Column3.DataPropertyName = "ToAddress";
+            this.Column3.HeaderText = "To Address";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Width = 170;
+            // 
+            // Column4
+            // 
+            this.Column4.DataPropertyName = "FromAddress";
+            this.Column4.HeaderText = "From Address";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            this.Column4.Width = 170;
+            // 
+            // Column6
+            // 
+            this.Column6.DataPropertyName = "ToName";
+            this.Column6.HeaderText = "To Name";
+            this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
+            this.Column6.Width = 170;
+            // 
+            // Column7
+            // 
+            this.Column7.DataPropertyName = "FromName";
+            this.Column7.HeaderText = "From Name";
+            this.Column7.Name = "Column7";
+            this.Column7.ReadOnly = true;
+            this.Column7.Width = 170;
+            // 
+            // Column5
+            // 
+            this.Column5.DataPropertyName = "Note";
+            this.Column5.HeaderText = "Note";
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            this.Column5.Width = 300;
+            // 
+            // Delete
+            // 
+            this.Delete.HeaderText = "DELETE";
+            this.Delete.Name = "Delete";
+            this.Delete.ReadOnly = true;
+            this.Delete.Text = "DELETE";
+            this.Delete.ToolTipText = "Delete";
+            this.Delete.UseColumnTextForButtonValue = true;
+            // 
+            // PostalViewPL
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.ClientSize = new System.Drawing.Size(1187, 530);
+            this.Controls.Add(this.dgvPostal);
+            this.Controls.Add(this.btnAddPostal);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExit);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "PostalViewPL";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PostalViewPL";
+            this.Load += new System.EventHandler(this.PostalViewPL_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPostal)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAddPostal;
+        private System.Windows.Forms.DataGridView dgvPostal;
+        private System.Windows.Forms.DataGridViewButtonColumn Edite;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewButtonColumn Delete;
+    }
+}
---
+++ HMS.PL/PostalViewPL.cs
@@ -0,0 +1,145 @@
+﻿using HMS.BLL;
+using HMS.DAL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HMS.PL
+{
+    public partial class PostalViewPL : Form
+    {
+        PostalBLL postalBLL;
+       
+        public PostalViewPL()
+        {
+            InitializeComponent();
+            postalBLL = new PostalBLL();
+
+            DataLoadGrid();
+            PageAccess();
+        }
+
+        public void PageAccess()
+        {
+            var res = LoginDAL.lstUserRole.Where(x => x.PageName == "Postal").FirstOrDefault();
+            if (res != null)
+            {
+                if (res.AddCommand == true)
+                {
+                    btnAddPostal.Visible = true;
+                }
+
+                if (res.EditCommand == true)
+                {
+                    dgvPostal.Columns[0].Visible = true;
+                }
+
+                if (res.DeleteCommand == true)
+                {
+                    dgvPostal.Columns[8].Visible = true;
+                }
+            }
+        }
+
+        public int DataLoadGrid()
+        {
+            
+                postalBLL.GetAllPostals(dgvPostal);
+                return 0;
+           
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnAddPostal_Click(object sender, EventArgs e)
+        {
+            foreach (Form f in this.MdiChildren)
+            {
+                if (f.GetType() == typeof(AddPostalPL))
+                {
+                    f.Activate();
+                    return;
+                }
+            }
+            foreach (Form f in Application.OpenForms)
+            {
+                if (f.GetType() == typeof(AddPostalPL))
+                {
+                    f.Activate();
+                    return;
+                }
+            }
+           
+            Form frm = new AddPostalPL(DataLoadGrid,0);
+            frm.MdiParent = MasterForm.ActiveForm;
+            frm.Show();
+        }
+
+        private void dgvPostal_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == dgvPostal.Columns["Edite"].Index && e.RowIndex >= 0)
+            {
+                foreach (Form f in this.MdiChildren)
+                {
+                    if (f.GetType() == typeof(AddPostalPL))
+                    {
+                        f.Activate();
+                        return;
+                    }
+                }
+                foreach (Form f in Application.OpenForms)
+                {
+                    if (f.GetType() == typeof(AddPostalPL))
+                    {
+                        f.Activate();
+                        return;
+                    }
+                }
+
+                int Id = Convert.ToInt32(dgvPostal.Rows[e.RowIndex].Cells[1].Value);
+                Form frm = new AddPostalPL(DataLoadGrid,Id);
+                frm.MdiParent = MasterForm.ActiveForm;
+                frm.Show();
+
+
+            }
+
+
+
+            if (e.ColumnIndex == dgvPostal.Columns["DELETE"].Index && e.RowIndex >= 0)
+            {
+
+                if (DialogResult.Yes == MessageBox.Show("Do You Want Delete ?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Warning))
+                {
+                    int Id = Convert.ToInt32(dgvPostal.Rows[e.RowIndex].Cells[1].Value);
+                    if (DeleteAppoiment(Id) > 0)
+                    {
+                        MessageBox.Show("Delete Succesfully", "System Alert", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        DataLoadGrid();
+                    }
+                }
+
+                
+            }
+        }
+
+        public int DeleteAppoiment(int id)
+        {
+            return postalBLL.DeletePostal(id);
+        }
+
+        private void PostalViewPL_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
